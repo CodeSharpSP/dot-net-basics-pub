08cacc24edd0d554f43d290dff02b20588e4a40c
dharmadurai03 <dharmakanagaraj@gmail.com>
2019-02-16 19:50:57 +0530

    Created new sample project then commiting the code

---
+++ Dharma/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int n;
+            Console.Write("enter the size of the array :\n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter value of index{0} ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum=0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+            Console.Write("sum of the array :{0}\n", sum);
+            int avg = sum / n;
+            Console.Write("average element of array:{0}\n", avg);
+            int min=a[0];
+            int mx=a[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] < min)
+                {
+                    min= a[i];
+                }
+                if (a[i] > mx)
+                {
+                    mx = a[i];
+                }
+            }
+            Console.Write("Minimum element is :{0}\n", min);
+            Console.Write("Maximum element is :{0}\n", mx);
+            Console.ReadKey();
+  }	
+    }
+}
---
+++ Dharma/ConsoleApplication2/ConsoleApplication2/sample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class sample
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("enter the row count:{0}\n");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the column count:{0}\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] a=new int[m,n];
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    Console.Write("Enter the value of {0}{0}:\n", i, j);
+                }
+            }
+
+
+
+           
+
+
+        }
+
+
+    }
+}
