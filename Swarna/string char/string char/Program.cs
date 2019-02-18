04135ee823c8fc2ef37b87830e4aad414a1840c3
swarnalakshmis <swarnalakshmi94@gmail.com>
2019-02-18 23:28:14 +0530

    commited

---
+++ Swarna/string char/string char/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Swarnalakshmi";
+            char[] chararray = sentence.ToCharArray();
+            foreach(char c in chararray)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+
+               
+        }
+    }
+}
---
+++ Swarna/string2/string2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1, rev = "";
+            int Length;
+            Console.WriteLine("Enter a string");
+            string1 = Console.ReadLine();
+            if (string1.Length == 10)
+            {
+                Length = string1.Length - 1;
+                while (Length >= 0)
+                {
+                    rev = rev + string1[Length];
+                    Length--;
+                }
+                Console.WriteLine("reversed text {0}", rev);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enter minimum of 10 letters of the text");
+            }
+            if(string1==rev)
+            {
+                Console.WriteLine("Yes same");
+            }
+            else
+            {
+                Console.WriteLine("Not same");
+            }
+        }
+    }
+}
